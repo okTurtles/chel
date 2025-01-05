01d5eec2d7ebd0794151a2c76de2f043a9f60285
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-01-05 21:50:06 +0000

    Add default multicode for hash

--- src/hash.ts
+++ src/hash.ts
@@ -1,11 +1,11 @@
 'use strict'
 
-import { createEntryFromFile } from './utils.ts'
+import { createEntryFromFile, multicodes } from './utils.ts'
 
 // TODO: use https://doc.deno.land/https://deno.land/std@0.140.0/streams/mod.ts/~/iterateReader instead to read in large files, and then use blake2b[Init,Update,Final] to iteratively calculate the hash
 //       Verify that it returns the same hash as when we use readAll https://doc.deno.land/https://deno.land/std@0.140.0/streams/mod.ts/~/readAll
 
-export async function hash (args: string[], multicode: number, internal = false) {
+export async function hash (args: string[], multicode: number = multicodes.RAW, internal = false) {
   const [filename] = args
   if (!filename) {
     console.error('please pass in a file')
