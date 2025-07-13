333eb0f6a7bb9863fe0a09d05ca1e7d4be42fd81
Akhilesh Thite <akhithite@gmail.com>
2025-07-13 13:24:54 -0700

    fix: improve error handling by using explicit error message cast

--- src/utils.ts
+++ src/utils.ts
@@ -84,7 +84,7 @@ export async function getBackend (src: string, { type, create } = { type: '', cr
   try {
     await backend.initStorage(initOptions)
   } catch (error) {
-    throw new Error(`could not init '${from}' storage backend at "${src}": ${error}`)
+    throw new Error(`could not init '${from}' storage backend at "${src}": ${(error as Error).message}`)
   }
   return backend
 }
