093b9203033028924ed3fe678b0a226ade6874e3
Akhilesh Thite <akhithite@gmail.com>
2025-07-17 16:53:40 -0700

    refactor: disable strict boolean and template expression rules for command handling

--- src/main.ts
+++ src/main.ts
@@ -15,21 +15,15 @@ import * as commands from './commands.ts'
 
 const [command, ...rest] = Deno.args
 
-if (typeof command !== 'string' || command.trim() === '') {
+// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+if (!command) {
   commands.help()
-} else if (
-  typeof command === 'string' &&
-  Object.prototype.hasOwnProperty.call(commands, command)
-) {
-  const cmdFn = commands[command as keyof typeof commands]
-  if (typeof cmdFn === 'function') {
-    await cmdFn(rest)
-  } else {
-    console.error(`Command exists but is not callable: ${String(command)}`)
-    Deno.exit(1)
-  }
+// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+} else if (commands[command as keyof typeof commands]) {
+  await commands[command as keyof typeof commands](rest)
 } else {
-  console.error(`Unknown command: ${String(command)}`)
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  console.error(`Unknown command: ${command}`)
   Deno.exit(1)
 }
 
