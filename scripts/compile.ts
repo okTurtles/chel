bbc1703293448f1b902ba2b5f44fd5be28bdf93d
Akhilesh Thite <akhithite@gmail.com>
2025-07-17 11:30:22 -0700

    fix: add type assertion to version import to avoid repeated casts

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -7,7 +7,7 @@ function $ (command: string) {
   return shell(command, { printOutput: true })
 }
 
-const { default: { version } } = await import('../package.json', { with: { type: 'json' } })
+const { default: { version } } = await import('../package.json', { with: { type: 'json' } }) as { default: { version: string } }
 
 export async function compile (): Promise<void> {
   const archs = ['x86_64-unknown-linux-gnu', 'aarch64-unknown-linux-gnu', 'x86_64-pc-windows-msvc', 'x86_64-apple-darwin', 'aarch64-apple-darwin']
@@ -17,9 +17,9 @@ export async function compile (): Promise<void> {
     // note: could also use https://examples.deno.land/temporary-files
     await $(`mkdir -vp ${dir}`)
     await $(`deno compile --allow-read=./ --allow-write=./  --allow-net -o ${dir}/${bin} --target ${arch} ./build/main.js`)
-    await $(`tar -C ./dist/tmp -czvf ./dist/chel-v${version as string}-${arch}.tar.gz ${arch}`)
+    await $(`tar -C ./dist/tmp -czvf ./dist/chel-v${version}-${arch}.tar.gz ${arch}`)
   }
-  await $(`sha256sum dist/chel-v${version as string}-*`)
+  await $(`sha256sum dist/chel-v${version}-*`)
   // TODO: sign the sha256sum! pipe this to gpg and include a link to your GPG key in the release notes!
 }
 
