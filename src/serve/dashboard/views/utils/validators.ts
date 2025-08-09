13a68ecdcf1dea0f8829bfbe4968c9a017dee396
Akhilesh Thite <akhithite@gmail.com>
2025-08-09 12:58:20 -0700

    fix(dashboard): use exact Vuelidate email regex for consistency

--- src/serve/dashboard/views/utils/validators.ts
+++ src/serve/dashboard/views/utils/validators.ts
@@ -7,7 +7,10 @@ export const required = (value: unknown): boolean => {
 }
 
 export const email = (value: string): boolean => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  // Using the exact same email regex as Vuelidate for consistency
+  // Source: https://github.com/vuelidate/vuelidate/blob/d028e164e0781ed2e716dbac77228a1443222366/packages/validators/src/raw/email.js
+  // deno-lint-ignore no-control-regex
+  const emailRegex = /^(?:[A-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[A-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]{2,}(?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i
   return emailRegex.test(value)
 }
 
