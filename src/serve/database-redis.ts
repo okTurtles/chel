b910407b405de83d39b1c54b1ae103420fd844e2
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-10-27 19:20:21 +0000

    Redis improvments

--- src/serve/database-redis.ts
+++ src/serve/database-redis.ts
@@ -1,41 +1,24 @@
 import { Buffer } from 'node:buffer'
-import { basename, dirname, resolve } from 'node:path'
-import { createClient, type RedisClientType } from 'npm:redis'
+import { RESP_TYPES, createClient, type RedisClientType } from 'npm:redis'
 import DatabaseBackend from './DatabaseBackend.ts'
 
 export default class RedisBackend extends DatabaseBackend {
-  dataFolder: string = ''
   db: RedisClientType | null = null
-  filename: string = ''
-  readStatement: { get: (key: string) => { value?: Buffer | string } | undefined } | null = null
-  writeStatement: { run: (key: string, value: Buffer | string) => unknown } | null = null
-  deleteStatement: { run: (key: string) => unknown } | null = null
+  url: string | undefined
 
-  constructor (options: { filepath?: string } = {}) {
+  constructor (options: { url?: string } = {}) {
     super()
-    const { filepath } = options
-    const resolvedPath = resolve(filepath!)
-    this.dataFolder = dirname(resolvedPath)
-    this.filename = basename(resolvedPath)
+    this.url = url
   }
 
   async init () {
-    this.db = createClient()
-    await this.db.connect()
-    /* const { dataFolder, filename } = this
-
-    await mkdir(dataFolder, { mode: 0o750, recursive: true })
-
-    if (this.db) {
-      throw new Error(`The ${filename} SQLite database is already open.`)
-    }
-    this.db = new sqlite.Database(join(dataFolder, filename))
-    this.run('CREATE TABLE IF NOT EXISTS Data(key TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL)')
-    console.info(`Connected to the ${filename} SQLite database.`)
-    this.readStatement = this.db.prepare('SELECT value FROM Data WHERE key = ?')
-    this.writeStatement = this.db.prepare('REPLACE INTO Data(key, value) VALUES(?, ?)')
-    this.deleteStatement = this.db.prepare('DELETE FROM Data WHERE key = ?')
-    */
+    this.db = createClient({
+      RESP: 3,
+      url: this.url
+    }).withTypeMapping({
+      [RESP_TYPES.BLOB_STRING]: Buffer
+    })
+    await this.db!.connect()
   }
 
   // Useful in test hooks.
