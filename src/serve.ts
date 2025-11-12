a4f3712a7b3823e34a1e955272664d10ecb1da15
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-11-12 19:24:18 +0000

    Lint 2

--- src/serve.ts
+++ src/serve.ts
@@ -19,6 +19,7 @@ async function startApplicationServer (): Promise<void> {
   await startServer.default
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function serve (_args: ArgumentsCamelCase<Params>) {
   try {
     // Start dashboard server on port 7000 first
